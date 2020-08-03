e7bb97288c931be3ec907b3dfd258a0482563222
twelsh37 <4956770+twelsh37@users.noreply.github.com>
2020-08-03 10:35:44 +0100

    Update dna.py

--- dna.py
+++ dna.py
@@ -1,34 +1,5 @@
-# Description: a program to work out who is identified by a DNA sequence
+# Description: A program to work out who is identified by a DNA sequence
 #
-# Specification
-# In a file called dna.py in ~/pset6/dna/, implement a program that identifies to whom a sequence of DNA belongs.
-#
-# 1.  The program should require as its first command-line argument the name of a CSV file containing the STR counts for
-# a # list of individuals and should require as its second command-line argument the name of a text file containing the
-# DNA sequence to identify.
-#
-# 1.1    If your program is executed with the incorrect number of command-line arguments, your program should print
-#     an error message of your choice (with print).# If the correct number of arguments are provided, you may
-#     assume that the first argument is indeed the filename of a # valid CSV file, and that the second argument is the
-#     filename of a valid text file.
-#
-# 2. Your program should open the CSV file and read its contents into memory.
-#
-# 2.2    You may assume that the first row of the CSV file will be the column names.# The first column will be the word
-#     'name' and the remaining columns will be the STR sequences themselves.
-
-# 3. Your program should open the DNA sequence and read its contents into memory.
-#
-# 4. For each of the STRs (from the first line of the CSV file), your program should compute the longest run of
-# consecutive repeats of the STR in the DNA sequence to identify.
-#
-# 5. If the STR counts match exactly with any of the individuals in the CSV file, your program should print out the name
-# of the matching individual.
-#
-# 5.5    You may assume that the STR counts will not match more than one individual.
-#     If the STR counts do not match exactly with any of the individuals in the CSV file, your program should print
-#     "No match".
-
 # DECONSTRUCTION:
 # Read in DB
 # Read in seq file
